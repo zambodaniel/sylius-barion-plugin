03428a835663cc2af2b5f627b79a1fbf1c7b526b
Daniel Zambo <zambodaniel@gmail.com>
2022-04-26 14:58:33 +0200

    Round total and get intval

--- src/Payum/BarionApi.php
+++ src/Payum/BarionApi.php
@@ -50,7 +50,7 @@ final class BarionApi
         $transaction = new \PaymentTransactionModel();
         $transaction->POSTransactionId = $payment->getId();
         $transaction->Payee = $this->payee;
-        $transaction->Total = intval($total / $divisor);
+        $transaction->Total = intval(round($total / $divisor));
 
         foreach ($payment->getOrder()->getItems() as $orderItem) {
             $item = new \ItemModel();
